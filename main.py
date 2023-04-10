402cc69dc1b1762c1976d8cae2485dd64f5a33bc
Erkka Heinilä <erkkahe@gmail.com>
2023-04-10 21:55:02 +0300

    Add computation and timing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.collections as clt
 from matplotlib.animation import FuncAnimation
 
 import numpy as np
+import time
 
 from automaton import Cell
 from automaton import State2D
@@ -15,22 +16,80 @@ if __name__ == "__main__":
 
 
     initial_pattern_str = """
-        wwwwwwwwwwwwwwwwwwwwww
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        wooooooxooooooooooooow
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        woooooooooxoooooooooow
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        woooooooooooooooooooow
-        wwwwwwwwwwwwwwwwwwwwww
+        wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooooooooooooooooxooooooooooooooooooooooooow
+        woooooxooooooooooooooooooooxooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooooooxooooooooooooxoooooooooooooooooooooow
+        wooooooooooooooooooooxoooxoooooooooooooooooooooooooooooooooxooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooxoooooooooooooooxooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooxooooooooooooooxooooooooooooooooooooooooxooooooxoooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooooooooooooooooooooooxooooooooooooooooooow
+        woooooooooooooooooooooooooooooxoooooooooooooxoooooooooooooooooooooooooow
+        woooooooooooooooooooooxooooooooooooooooooooooooooooooxooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooxoooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooxoooooooooooxooooooooooooooooooooooooxoooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooooooooooooooooooooooooooxooooooooooooooow
+        wooooooooooooooooooooooooooooooxooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooxoooooooxoooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooxoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooxooooooooooooxooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooooooxooooooooooooooxooooooooxooooooooooooooooooooow
+        wooooooooooooooooooooooooooooxooooooooooooooooooooooooooooooooooooooooow
+        woooooooxooooooooooooooooooooooooooooooooooooooooooooooxooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooxooooooooxoooooooooxooooooooxoooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooxoooooooooooooooxooooooooooooooooooooxoooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooxoooooooooooooooooooooooooooooooooxooooooooooooooow
+        wooooooooooooxooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooxooooooooooooooooxooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooxoooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooooooooooooooooooooooxooooooooooooooooooow
+        woooooooooooooooxoooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooxooooooooooooooooooooxooooooooxooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooxoooooooooooooooooooooooxooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooxooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooooooxoooooooooooooooxooooooooooooooooooow
+        woooooooooooooooooooxoooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooxooooxoooooooooooooooooxooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooxoooooooooooooooooooooooooooxooooooooooxooooooooooooooooooow
+        woooooooooooooooooooooooxooooooooooooooooooooooooooooooooooxooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooxooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooooooooooooooooooxooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooxooooooooooooooooooooooooooxoooooxooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wooooooooooooooooooooxoooooooooooooooooooooooooooooooooxooooooooooooooow
+        woooooooooooooooooooooooooooooooxoooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooooooxooooooooooooooooooooooxooooooooooooooow
+        wooooooooooooooooooooooooooooooooooooooooooooooxooooooooooooooooooooooow
+        wooooooooooooooooooooooooooooooooooooooooooooooooooooxooooooooooooooooow
+        woooooooooooxoooooooooooooooooxoooooooooooooooooooooooooooooooooooooooow
+        woooooooooooooooooooooooooooxooooooooooooooooooooooooxooooooooooooooooow
+        woooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow
+        wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
     """
 
     initial_pattern = []
@@ -71,17 +130,22 @@ if __name__ == "__main__":
                 1,
             )
             paths.append(rect)
-    collection = clt.PatchCollection(paths, edgecolors='green', facecolors=['white'], linewidths=1)
+    collection = clt.PatchCollection(paths, edgecolors='black', facecolors=['white'], linewidths=0.1)
     ax.add_collection(collection)
 
     def animation_func(idx):
         """Helper to update a single frame."""
+
+        # time it
+        beginning = time.time()
 
         # Get the global state
         global state
 
         # And update it via the automaton rule.
         state = rule.apply(state, idx)
+
+        middlepoint = time.time()
 
         # Figure out the color of each cell
         colors = []
@@ -97,6 +161,10 @@ if __name__ == "__main__":
                 colors.append(facecolor)
         # And only update them
         collection.set_facecolors(colors)
+
+        # timing information
+        times = [middlepoint - beginning, time.time() - middlepoint]
+        print(f"Computation: {times[0]}, plotting: {times[1]}")
 
     # Let matplotlib do the real work
     ani = FuncAnimation(fig, animation_func, frames=1000, interval=0, blit=False)
